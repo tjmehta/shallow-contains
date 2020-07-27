202126f00185ff8447bcfa1e992c2181942e71bf
Tejesh Mehta <tejesh.mehta@gmail.com>
2020-07-26 21:14:58 -0700

    initial implementation with 100% test coverage

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,1 +1,308 @@
+import shallowContains from '../index'
 
+const comparator = (a: any, b: any): boolean => (b.test ? b.test(a) : a === b)
+
+describe('shallowContains', () => {
+  describe('array shallow contains...', () => {
+    it('should contain all values in contains array', () => {
+      expect(shallowContains([1, 2, 3], [3, 2, 1])).toBe(true)
+      expect(shallowContains([1, 2, 3], [3, 2])).toBe(true)
+      expect(shallowContains([1, 2, 3], [1])).toBe(true)
+      expect(shallowContains([1, 2, 3], [1, 2, 4])).toBe(false)
+      expect(shallowContains([1, 2, 3], [1, 4])).toBe(false)
+      expect(shallowContains([1, 2, 3], [4])).toBe(false)
+      expect(shallowContains([1, 2, 3], [/2/], comparator)).toBe(true)
+    })
+
+    it('should contain all values in contains set', () => {
+      expect(shallowContains([1, 2, 3], new Set([3, 2, 1]))).toBe(true)
+      expect(shallowContains([1, 2, 3], new Set([3, 2]))).toBe(true)
+      expect(shallowContains([1, 2, 3], new Set([1]))).toBe(true)
+      expect(shallowContains([1, 2, 3], new Set([1, 2, 4]))).toBe(false)
+      expect(shallowContains([1, 2, 3], new Set([1, 4]))).toBe(false)
+      expect(shallowContains([1, 2, 3], new Set([4]))).toBe(false)
+    })
+  })
+
+  describe('set shallow contains...', () => {
+    it('should contain all values in contains array', () => {
+      expect(shallowContains(new Set([1, 2, 3]), [3, 2, 1])).toBe(true)
+      expect(shallowContains(new Set([1, 2, 3]), [3, 2])).toBe(true)
+      expect(shallowContains(new Set([1, 2, 3]), [1])).toBe(true)
+      expect(shallowContains(new Set([1, 2, 3]), [4, 2, 1])).toBe(false)
+      expect(shallowContains(new Set([1, 2, 3]), [1, 4])).toBe(false)
+      expect(shallowContains(new Set([1, 2, 3]), [4])).toBe(false)
+    })
+
+    it('should contain all values in contains set', () => {
+      expect(shallowContains(new Set([1, 2, 3]), new Set([3, 2, 1]))).toBe(true)
+      expect(shallowContains(new Set([1, 2, 3]), new Set([3, 2]))).toBe(true)
+      expect(shallowContains(new Set([1, 2, 3]), new Set([1]))).toBe(true)
+      expect(shallowContains(new Set([1, 2, 3]), new Set([3, 2, 4]))).toBe(
+        false,
+      )
+      expect(shallowContains(new Set([1, 2, 3]), new Set([3, 4]))).toBe(false)
+      expect(shallowContains(new Set([1, 2, 3]), new Set([4]))).toBe(false)
+    })
+  })
+
+  describe('obj shallow contains...', () => {
+    it('should contain all values in contains array', () => {
+      expect(shallowContains({ foo: 1, bar: 2, qux: 3 }, [3, 2, 1])).toBe(true)
+      expect(shallowContains({ foo: 1, bar: 2, qux: 3 }, [3, 2])).toBe(true)
+      expect(shallowContains({ foo: 1, bar: 2, qux: 3 }, [1])).toBe(true)
+      expect(shallowContains({ foo: 1, bar: 2, qux: 3 }, [3, 2, 4])).toBe(false)
+      expect(shallowContains({ foo: 1, bar: 2, qux: 3 }, [3, 4])).toBe(false)
+      expect(
+        shallowContains({ foo: 1, bar: 2, qux: 3 }, [/4/], comparator),
+      ).toBe(true)
+    })
+
+    it('should contain all values in contains set', () => {
+      expect(
+        shallowContains({ foo: 1, bar: 2, qux: 3 }, new Set([3, 2, 1])),
+      ).toBe(true)
+      expect(shallowContains({ foo: 1, bar: 2, qux: 3 }, new Set([3, 2]))).toBe(
+        true,
+      )
+      expect(shallowContains({ foo: 1, bar: 2, qux: 3 }, new Set([1]))).toBe(
+        true,
+      )
+      expect(
+        shallowContains({ foo: 1, bar: 2, qux: 3 }, new Set([3, 2, 4])),
+      ).toBe(false)
+      expect(shallowContains({ foo: 1, bar: 2, qux: 3 }, new Set([3, 4]))).toBe(
+        false,
+      )
+      expect(shallowContains({ foo: 1, bar: 2, qux: 3 }, new Set([4]))).toBe(
+        false,
+      )
+    })
+
+    it('should contain all values in contains obj', () => {
+      expect(
+        shallowContains({ foo: 1, bar: 2, qux: 3 }, { foo: 1, bar: 2, qux: 3 }),
+      ).toBe(true)
+      expect(
+        shallowContains({ foo: 1, bar: 2, qux: 3 }, { foo: 1, bar: 2 }),
+      ).toBe(true)
+      expect(shallowContains({ foo: 1, bar: 2, qux: 3 }, { foo: 1 })).toBe(true)
+      expect(
+        shallowContains({ foo: 1, bar: 2, qux: 3 }, { foo: 1, bar: 2, qux: 4 }),
+      ).toBe(false)
+      expect(
+        shallowContains({ foo: 1, bar: 2, qux: 3 }, { foo: 1, bar: 4 }),
+      ).toBe(false)
+      expect(shallowContains({ foo: 1, bar: 2, qux: 3 }, { foo: 4 })).toBe(
+        false,
+      )
+      expect(
+        shallowContains({ foo: 1, bar: 2, qux: 3 }, { foo: /1/ }, comparator),
+      ).toBe(true)
+    })
+
+    it('should contain all values in contains map', () => {
+      expect(
+        shallowContains(
+          { foo: 1, bar: 2, qux: 3 },
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+        ),
+      ).toBe(true)
+      expect(
+        shallowContains(
+          { foo: 1, bar: 2, qux: 3 },
+          new Map(Object.entries({ foo: 1, bar: 2 })),
+        ),
+      ).toBe(true)
+      expect(
+        shallowContains(
+          { foo: 1, bar: 2, qux: 3 },
+          new Map(Object.entries({ foo: 1 })),
+        ),
+      ).toBe(true)
+      expect(
+        shallowContains(
+          { foo: 1, bar: 2, qux: 3 },
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 4 })),
+        ),
+      ).toBe(false)
+      expect(
+        shallowContains(
+          { foo: 1, bar: 2, qux: 3 },
+          new Map(Object.entries({ foo: 1, bar: 4 })),
+        ),
+      ).toBe(false)
+      expect(
+        shallowContains(
+          { foo: 1, bar: 2, qux: 3 },
+          new Map(Object.entries({ foo: 4 })),
+        ),
+      ).toBe(false)
+    })
+  })
+
+  describe('map shallow contains...', () => {
+    it('should contain all values in contains array', () => {
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), [
+          3,
+          2,
+          1,
+        ]),
+      ).toBe(true)
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), [
+          3,
+          2,
+        ]),
+      ).toBe(true)
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), [
+          1,
+        ]),
+      ).toBe(true)
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), [
+          3,
+          2,
+          4,
+        ]),
+      ).toBe(false)
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), [
+          3,
+          4,
+        ]),
+      ).toBe(false)
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), [
+          4,
+        ]),
+      ).toBe(false)
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          { foo: /1/ },
+          comparator,
+        ),
+      ).toBe(true)
+    })
+
+    it('should contain all values in contains set', () => {
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Set([3, 2, 1]),
+        ),
+      ).toBe(true)
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Set([3, 2]),
+        ),
+      ).toBe(true)
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Set([1]),
+        ),
+      ).toBe(true)
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Set([3, 2, 4]),
+        ),
+      ).toBe(false)
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Set([3, 4]),
+        ),
+      ).toBe(false)
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Set([4]),
+        ),
+      ).toBe(false)
+    })
+
+    it('should contain all values in contains obj', () => {
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), {
+          foo: 1,
+          bar: 2,
+          qux: 3,
+        }),
+      ).toBe(true)
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), {
+          foo: 1,
+          bar: 2,
+        }),
+      ).toBe(true)
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), {
+          foo: 1,
+        }),
+      ).toBe(true)
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), {
+          foo: 1,
+          bar: 2,
+          qux: 4,
+        }),
+      ).toBe(false)
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), {
+          foo: 1,
+          bar: 4,
+        }),
+      ).toBe(false)
+      expect(
+        shallowContains(new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })), {
+          foo: 4,
+        }),
+      ).toBe(false)
+    })
+
+    it('should contain all values in contains map', () => {
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+        ),
+      ).toBe(true)
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Map(Object.entries({ foo: 1, bar: 2 })),
+        ),
+      ).toBe(true)
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Map(Object.entries({ foo: 1 })),
+        ),
+      ).toBe(true)
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 4 })),
+        ),
+      ).toBe(false)
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Map(Object.entries({ foo: 1, bar: 4 })),
+        ),
+      ).toBe(false)
+      expect(
+        shallowContains(
+          new Map(Object.entries({ foo: 1, bar: 2, qux: 3 })),
+          new Map(Object.entries({ foo: 4 })),
+        ),
+      ).toBe(false)
+    })
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+export default function shallowContains(
+  input: any,
+  contains: {} | Array<any> | Set<any> | Map<any, any>,
+  comparator?: (a: any, b: any) => boolean,
+) {
+  const equals = comparator || ((a, b) => a === b)
+
+  if (Array.isArray(contains) || contains instanceof Set) {
+    const inputSet = toSet(input)
+    for (let v of contains) {
+      if (!comparator) {
+        if (!inputSet.has(v)) return false
+      } else {
+        for (let inputVal of inputSet) {
+          if (equals(inputVal, v)) return true
+        }
+      }
+    }
+    return true
+  }
+
+  const containsEntries = toEntries(contains)
+  const inputMap = toMap(input)
+  for (let [k, v] of containsEntries) {
+    if (!equals(inputMap.get(k), v)) return false
+  }
+  return true
+}
+
+function toSet(val: any): Set<any> {
+  if (Array.isArray(val)) return new Set(val)
+  if (val instanceof Set) return val
+  if (val instanceof Map) return new Set(val.values())
+  // @ts-ignore
+  return new Set(Object.values(val))
+}
+
+function toMap(val: any) {
+  if (val instanceof Map) return val
+  // @ts-ignore
+  return new Map(Object.entries(val))
+}
+
+function toEntries(val: any) {
+  // @ts-ignore
+  return val.entries ? val.entries() : Object.entries(val)
+}
